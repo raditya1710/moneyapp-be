76b8abaeb57c7e724b7e0ae4c6e66ffe0c4bcd4d
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2019-07-09 23:38:07 +0700

    Rename fields to camelCase

--- transaction/models.go
+++ transaction/models.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Transaction struct {
-	Title string
-	Amount float64
-	DateFulfilled time.Time
-	DateCreated time.Time
-	DateUpdated time.Time
+	title string
+	amount float64
+	dateFulfilled time.Time
+	dateCreated time.Time
+	dateUpdated time.Time
 }
